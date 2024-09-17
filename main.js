58f86d99d6445d818d3929a052a730b3d53df1e1
tammerb <tammer.barkouki@gmail.com>
2024-09-17 12:27:13 -0700

    new survey links

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const links = [
-    "https://thbarkouki.limesurvey.net/954158?lang=en",
-    "https://thbarkouki.limesurvey.net/215748?lang=en"
+    "https://thbarkouki.limesurvey.net/724641?lang=en",
+    "https://thbarkouki.limesurvey.net/236747?lang=en",
+    "https://thbarkouki.limesurvey.net/898877?lang=en",
+    "https://thbarkouki.limesurvey.net/317844?lang=en",
+    "https://thbarkouki.limesurvey.net/448153?lang=en",
+    "https://thbarkouki.limesurvey.net/178952?lang=en",
     ];
 
 function redirectToRandomWebsite() {
